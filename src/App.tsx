d00c28c836842e8f106690d28606d0676efab6a0
Demir Boraj <demirboraj97@gmail.com>
2022-08-18 18:19:21 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [coinSide, setCoinSide] = useState( "heads" || "tails" );
+
+    
+    function flipCoin() {
+      let coinSide =Math.random()
+      console.log(coinSide)
+      if(coinSide<=0.5){
+        setCoinSide("Head")
+      }else setCoinSide("Tails")
+    }
+
+
+ 
+
+  return (
+    <div className="App">
+ 
+      <h1>Flip a Coin</h1>
+      <p className="coin">{coinSide}</p>
+  <button onClick={flipCoin}>Flip Coin</button>
+
+
+
+    </div>
+  );
+}
+
+export default App;
